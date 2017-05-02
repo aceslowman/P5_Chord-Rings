7831ae00dd89cd69c3d3a8e84b025c5c6e9a72f7
aceslowman <aceslowman@gmail.com>
2017-05-02 16:09:51 -0600

    Added some gui elements, allows for osc mode switching, annd now changes pitch linearly

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,7 @@
 var rings = [];
 var driver;
+var paused;
+var osc_type = "square";
 
 function setup(){
   //create our canvas, which will match our window width and height
@@ -9,18 +11,30 @@ function setup(){
   ellipseMode(RADIUS);
 
   for(var i = 0; i < 5; i++){
-    rings.push(new Ring(50*(i+1)));
+    rings.push(new Ring(80*(i+1)));
   }
+
+  paused = true;
 }
 
 function draw(){
   //clear the screen for each frame
   background(255);
 
+  stroke(0);
+  line(width/2,height/2,width/2 + (80*rings.length),height/2);
+  text("Θ = 0", width/2 + (80*rings.length) + 30, height/2 + 4);
+
+
+
   for(var i = 0; i < rings.length; i++){
     rings[i].update();
     rings[i].display();
   }
+
+
+  drawInstructions();
+  drawOscTypes();
 }
 
 function Ring(size){
@@ -32,7 +46,7 @@ function Ring(size){
   this.ctr_position.y = this.radius*sin(this.theta) + this.center.y;
 
   this.osc = new p5.Oscillator();
-  this.osc.setType('sine');
+  this.osc.setType(osc_type);
   this.osc.freq(0);
   this.osc.amp(0.2);
   this.osc.start();
@@ -49,12 +63,18 @@ function Ring(size){
     noFill();
     ellipse(this.center.x,this.center.y,this.radius);
 
-    fill(255);
+    if(this.selected){
+      fill(0);
+    }else{
+      fill(255);
+    }
+
     ellipse(this.ctr_position.x,this.ctr_position.y,10);
     noFill();
-    
+
     textAlign(CENTER);
     text(Math.floor(this.frequency)+" hz", this.ctr_position.x, this.ctr_position.y-15);
+    text("Θ = "+this.theta.toFixed(2), this.ctr_position.x, this.ctr_position.y+25);
     // text(Math.floor(this.theta * 180 / Math.PI)+180, this.ctr_position.x, this.ctr_position.y-15);
   }
 
@@ -66,8 +86,10 @@ function Ring(size){
       this.offset = Math.atan2(mouseY - this.center.y, mouseX - this.center.x);
     }else{
       //the existing theta
-      this.driver++;
-      this.theta = this.driver/100;
+      if(!paused){
+        this.driver++;
+      }
+              this.theta = this.driver/100;
     }
 
     this.ctr_position.x = this.radius * cos(this.theta + this.offset) + this.center.x;
@@ -76,7 +98,8 @@ function Ring(size){
     this.theta = Math.atan2(this.ctr_position.y - this.center.y, this.ctr_position.x - this.center.x);
 
     // convert theta to frequency
-    this.frequency = (((sin(this.theta)+1)/2)*100)+200;
+    // this.frequency = (((sin(this.theta)+1)/2)*100)+200;
+    this.frequency = (Math.abs(this.theta) * 200);
     this.osc.freq(this.frequency);
   }
 }
@@ -99,4 +122,64 @@ function mouseReleased(){
       rings[i].selected = false;
     }
   }
+}
+
+function keyPressed(){
+  if(key == " "){
+    paused = !paused;
+  }
+}
+
+function keyTyped(){
+  if(key == 1){
+    osc_type = "triangle";
+  }else if(key == 2){
+    osc_type = "sine";
+  }else if(key == 3){
+    osc_type = "square";
+  }
+
+  if(key == "0"){
+    for(var i = 0; i < rings.length; i++){
+      rings[i].theta = 0;
+      rings[i].driver = 0;
+      rings[i].offset = 0;
+    }
+  }
+
+  for(var i = 0; i < rings.length; i++){
+    rings[i].osc.setType(osc_type);
+  }
+}
+
+function drawOscTypes(){
+  translate(width/2,height/2);
+  fill(255);
+
+  var s_size = 10;
+
+  switch (osc_type) {
+    case "triangle":
+      triangle(
+        -s_size , s_size,
+        s_size , s_size,
+        0, -s_size
+      )
+      break;
+    case "square":
+      quad(-s_size,-s_size,-s_size,s_size,s_size,s_size,s_size,-s_size)
+      break
+    case "sine":
+      ellipse(0,0,s_size)
+      break;
+  }
+  noFill();
+}
+
+function drawInstructions(){
+  textAlign(LEFT);
+  text("Press 0 to return theta to 0",50,50);
+  text("Press Spacebar to pause",50,100);
+  text("Press 1,2,3 to cycle oscillators",50,150);
+  textAlign(CENTER);
 }
